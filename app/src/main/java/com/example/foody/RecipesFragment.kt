f45b81010ca06e712baf4a95639cab0dadeb11c2
makikaka <mladen.tasevski@stage.alosys.it>
2021-08-11 02:19:37 +0200

    Remove shimmer default

--- app/src/main/java/com/example/foody/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/RecipesFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 class RecipesFragment : Fragment() {
 
@@ -14,9 +13,7 @@ class RecipesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
-        view.recycler_view.showShimmer();
-        return view;
+        return inflater.inflate(R.layout.fragment_recipes, container, false)
     }
 
 }
