d435d8d31d3dc0762a1620d9a1049531cb927c73
makikaka <mladen.tasevski@stage.alosys.it>
2021-08-12 21:24:06 +0200

    Reorganize project structure, add constants

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.foody;
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "e7a6f4e5fd844c76807687db194d8990";
+        const val BASE_URL = "https://api.spoonacular.com";
+    }
+}
--- app/src/main/java/com/example/foody/models/Result.kt
+++ app/src/main/java/com/example/foody/models/Result.kt
@@ -1,4 +1,4 @@
-package com.example.foody
+package com.example.foody.models
 
 
 import com.google.gson.annotations.SerializedName
