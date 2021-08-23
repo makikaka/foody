6571b461cdf8c9cfb8e4c81f5af4311b7451e201
makikaka <mladen.tasevski@stage.alosys.it>
2021-08-23 18:19:58 +0200

    Move query constants to recipesViewModel

--- app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -8,17 +8,20 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.foody.MainViewModel
+import com.example.foody.viewmodels.MainViewModel
 import com.example.foody.R
 import com.example.foody.adapters.RecipesAdapter
 import com.example.foody.util.Constants.Companion.API_KEY
 import com.example.foody.util.NetworkResult
+import com.example.foody.viewmodels.RecipesViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_recipes.view.*
 @AndroidEntryPoint
 class RecipesFragment : Fragment() {
 
     private lateinit var mainViewModel: MainViewModel
+
+    private lateinit var recipesViewModel: RecipesViewModel
     private lateinit var mView: View
     private val mAdapter by lazy { RecipesAdapter() }
 
@@ -30,6 +33,7 @@ class RecipesFragment : Fragment() {
         mView =  inflater.inflate(R.layout.fragment_recipes, container, false)
 
         mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        recipesViewModel = ViewModelProvider(requireActivity()).get(RecipesViewModel::class.java)
 
         setupRecyclerView()
         requestApiData()
@@ -44,7 +48,7 @@ class RecipesFragment : Fragment() {
     }
 
     private fun requestApiData() {
-        mainViewModel.getRecipes(applyQueries())
+        mainViewModel.getRecipes(recipesViewModel.applyQueries())
         mainViewModel.recipesResponse.observe(viewLifecycleOwner, {
             response ->
             when(response) {
@@ -67,19 +71,6 @@ class RecipesFragment : Fragment() {
                 }
             }
         })
-    }
-
-    private fun applyQueries(): HashMap<String, String> {
-        val queries: HashMap<String, String> = HashMap()
-
-        queries["number"] = "50";
-        queries["apiKey"] = API_KEY;
-        queries["type"] = "snack";
-        queries["diet"] = "vegan"
-        queries["addRecipeInformation"] = "true"
-        queries["fillIngredients"] = "true"
-
-        return queries;
     }
 
     private fun showShimmerEffect () {
--- app/src/main/java/com/example/foody/util/Constants.kt
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -5,5 +5,13 @@ class Constants {
     companion object {
         const val API_KEY = "e7a6f4e5fd844c76807687db194d8990";
         const val BASE_URL = "https://api.spoonacular.com";
+
+        // API query keys
+        const val QUERY_NUMBER = "number"
+        const val QUERY_API_KEY = "apiKey"
+        const val QUERY_TYPE = "type"
+        const val QUERY_DIET = "diet"
+        const val QUERY_ADD_RECIPES_INFORMATION = "addRecipeInformation"
+        const val QUERY_FILL_INGREDIENTS = "fillIngredients"
     }
 }
